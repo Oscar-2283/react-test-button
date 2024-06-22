efd7dcdc03d0476a6d68ea3f07209114747fce4b
Oscar Wang <>
2024-06-22 14:18:11 +0800

    update

---
+++ src/Button.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Button from '@mui/material/Button';
+import ExpandLessIcon from '@mui/icons-material/ExpandLess';
+
+const TestButton = () => {
+  return (
+    <Button variant="contained" color="primary">
+      <ExpandLessIcon />
+    </Button>
+  );
+};
+
+export default TestButton;
---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'production',
+  entry: './src/Button.js',
+  output: {
+    path: path.resolve('lib'),
+    filename: 'Button.js',
+    libraryTarget: 'commonjs2',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-react', '@babel/preset-env']
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ],
+  },
+  resolve: {
+    alias: {
+      react: path.resolve(__dirname, './node_modules/react'),
+      'react-dom': path.resolve(__dirname, './node_modules/react-dom'),
+    },
+    extensions: ['.js', '.jsx']
+  },
+  externals: {
+    react: {
+      commonjs: "react",
+      commonjs2: "react",
+      amd: "React",
+      root: "React"
+    },
+    'react-dom': {
+      commonjs: "react-dom",
+      commonjs2: "react-dom",
+      amd: "ReactDOM",
+      root: "ReactDOM"
+    }
+  }
+};
